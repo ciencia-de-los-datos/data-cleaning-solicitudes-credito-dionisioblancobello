044ba4115e505b820eabeaa945101bf56544a062
dionisioblancobello <diblancob@unal.edu.co>
2022-10-21 14:18:16 -0500

    intento3

--- pregunta.py
+++ pregunta.py
@@ -20,13 +20,13 @@ def clean_data():
 
     #eliminar datos faltantes y duplicados
     df.dropna(inplace=True)
-    df.drop_duplicates(inplace=True)
+    
 
     #volver todo minusculas y eliminar espacios al inicio y al final
     df["sexo"] = df["sexo"].apply(lambda x: str(x).lower().strip())
     df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x: str(x).lower().strip())
     df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().strip())
-    df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().strip())
+    #df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().strip())
     #df["estrato"] = df["estrato"].apply(lambda x: str(x).lower().strip())
     #df["comuna_ciudadano"] = df["comuna_ciudadano"].apply(lambda x: str(x).lower().strip())
     #df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(lambda x: str(x).lower().strip())
@@ -40,7 +40,7 @@ def clean_data():
     df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
 
     #modificar formato fecha
-    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"])
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
 
     #monto eliminar signo $, y limpiar
     df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
@@ -52,5 +52,3 @@ def clean_data():
     df.dropna(inplace=True)
 
     return df
-
-print(clean_data())
